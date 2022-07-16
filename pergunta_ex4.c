c6db9bb8bf0456af4b1b3d17584bd9f6a5e086c8
Marcos Chalet <chaletmarcos@gmail.com>
2022-07-16 11:06:05 -0300

    add: exercício 4

---
+++ pergunta_ex4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+   
+   char palavra[] = {"Uma String Qualquer"};
+
+   /*
+    * Procedimento 1
+    * */
+
+
+   /*
+    * Procedimento 2
+    * */
+
+   printf("%s\n", palavra);
+
+   return 0;
+}
