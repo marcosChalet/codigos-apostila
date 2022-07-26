f6952826d8546dd3feb8cd4b0efdd4609cba72a2
Marcos Chalet <chaletmarcos@gmail.com>
2022-07-26 12:08:26 -0300

    add: exercício 4

---
+++ registros/pergunta_ex4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct curso {
+   char nomeCurso[50];
+   char nomeUniversidade[50];
+}Curso;
+
+typedef struct pessoa {
+   int id;
+   Curso* formacao;
+}Pessoa;
+
+int main () {
+
+   /*
+    * Procedimento
+    * */
+
+   return 0;
+}
---
+++ registros/resposta_ex4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct curso {
+   char nomeCurso[50];
+   char nomeUniversidade[50];
+}Curso;
+
+typedef struct pessoa {
+   int id;
+   Curso* formacao;
+}Pessoa;
+
+int main () {
+
+   Pessoa* pessoa = (Pessoa*)malloc(sizeof(Pessoa));
+   if (pessoa == NULL) {
+      perror("Erro ao alocar memória");
+      exit(1);
+   }
+
+   Curso curso;
+   pessoa->formacao = &curso;
+
+   /*
+    * Como os campos de 'pessoa' são ponteiros
+    * usamos as setas.
+    * */
+
+   pessoa->id = 1;
+   strcpy(pessoa->formacao->nomeCurso, "Ciência da Computação");
+   strcpy(pessoa->formacao->nomeUniversidade, "Universidade Federal do Cariri");
+
+   printf("Id: %d\n", pessoa->id);
+   printf("Curso: %s\n", pessoa->formacao->nomeCurso);
+   printf("Universidade: %s\n", pessoa->formacao->nomeUniversidade);
+
+   return 0;
+}
